c040a0c0d8c34f849b296240939ec835efa1c80d
Chris Koorzen <chriskoorzendev@gmail.com>
2022-11-10 09:57:35 +0000

    add basic Database connection tests

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 """Test program"""
 from databaseManagers import Database
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 db = Database
 
-db.connectDB('elory.db')        # Connecting to "foreign" database
+# --- DB Connection Tests ---
 
-db.FileManager.getFilesfromTags()
+# db.connectDB('root/screenshot.jpg')        # Connecting to file that is not sqlite db
+# db.connectDB('unknown.db')                 # Connecting to database that does not exist (new file)
+# add -> Connecting to database that does exist, but is empty
+# add -> Connecting to database that does exist, tables match but columns do not
+# add -> Connecting to database that does exist, tables match and columns match
+# db.connectDB('elory.db')                   # Connecting to "foreign" database, tables match but columns do not
+# add -> Connecting to "foreign" database, tables match and columns match
 
-print(db.TagManager.tags)
+
+# db.FileManager.getFilesfromTags()
+# print(db.TagManager.tags)
+
+print("end of run.")
