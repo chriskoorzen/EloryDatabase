98730988870b5c6a4d0c10a39b3b3e668ea78ddb
Chris Koorzen <chriskoorzendev@gmail.com>
2022-11-12 11:14:23 +0000

    Add basic Kivy GUI framework

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from databaseManagers import Database
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+from databaseManagers import Database as db
 import logging
-
 logging.basicConfig(level=logging.DEBUG)
-
-db = Database
 
 db.connect_db('database/known.db')          # Connecting to database that does exist, tables match and columns match
 
 
-print("\ntags: ")
-for i in db.TagManager.tags.values():
-    print(i)
-print("\ngroups: ")
-for i in db.TagManager.groups.values():
-    print(i)
-print("\nfiles: ")
-for i in db.FileManager.files.values():
-    print(i)
+class RootWidget(Widget):
+    pass
 
+
+class EloryApp(App):
+    def build(self):
+        return RootWidget()
+
+
+if __name__ == "__main__":
+    EloryApp().run()
