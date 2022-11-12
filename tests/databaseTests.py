6228fca74643b682593b6267a7aa762640f845a4
Chris Koorzen <chriskoorzendev@gmail.com>
2022-11-12 11:12:49 +0000

    Add some basic connection and database creation tests

--- tests/databaseTests.py
+++ tests/databaseTests.py
@@ -1,17 +1,57 @@
 """Test program"""
 import unittest
 import sqlite3
-from databaseManagers import Database
+import os
+from databaseManagers import Database, db_logger
 
 
+# --- DB Connection Tests ---
 class TestDataBaseConnections(unittest.TestCase):
-    # --- DB Connection Tests ---
-    def test_reject_invalid_file_connection(self):
-        with self.assertRaises(sqlite3.DatabaseError):
-            Database.connect_db('root/screenshot.jpg')        # Connecting to file that is not sqlite db
 
-    # db.connectDB('unknown.db')                 # Connecting to database that does not exist (new file)
-    # add -> Connecting to database that does exist, but is empty
+    # Case 1: Connecting to database that does not exist (new file)
+    def test_create_new_file_if_file_not_exists(self):
+        non_existent_db = 'database/does_not_exist.db'
+        self.assertFalse(os.path.isfile(non_existent_db))   # File does not exist
+        with self.assertLogs(db_logger, "WARNING") as lg:
+            Database.connect_db(non_existent_db)
+
+        # Expecting 2 Warning messages
+        self.assertEqual(lg.records[0].getMessage(),
+            f"File '{non_existent_db}' does not exist. Creating new sqlite3 database")
+        self.assertEqual(lg.records[1].getMessage(),
+            f"Database '{non_existent_db}' is empty... Auto creating tables...")
+        self.assertTrue(os.path.isfile(non_existent_db))    # File exists
+        self.addCleanup(os.remove, non_existent_db)
+
+    # Case 2: Connecting to file that is not sqlite db
+    def test_reject_invalid_file_connection(self):
+        invalid_file = 'database/screenshot.jpg'
+        self.assertTrue(os.path.isfile(invalid_file))       # File exists
+        with self.assertRaises(sqlite3.DatabaseError):
+            Database.connect_db(invalid_file)
+
+    # Case 3: Connected to empty file ->
+    def test_create_new_database_if_file_empty(self):
+        empty_file = 'database/empty_file'
+        with open(empty_file, 'w') as f:                    # Create empty file
+            f.close()
+        self.assertTrue(os.path.isfile(empty_file))         # Exists
+        with open(empty_file, 'r') as f:
+            data = f.read()
+            self.assertEqual(len(data), 0)                  # File is empty
+        with self.assertLogs(db_logger, "WARNING") as lg:   # Issues warning
+            Database.connect_db(empty_file)
+        self.assertEqual(lg.records[0].getMessage(),        # Expecting 1 Warning message
+            f"Database '{empty_file}' is empty... Auto creating tables...")
+        CON = sqlite3.connect(empty_file)
+        CUR = CON.cursor()
+        CUR.execute("PRAGMA schema_version")
+        self.assertNotEqual(0, CUR.fetchone()[0])           # This is a valid sqlite3 db -> returns 0 otherwise
+        CON.close()
+        self.addCleanup(os.remove, empty_file)
+
+
+    # db.connectDB('unknown.db')
     # add -> Connecting to database that does exist, tables match but columns do not
     # db.connectDB('database/known.db')          # Connecting to database that does exist, tables match and columns match
     # db.connectDB('elory.db')                   # Connecting to "foreign" database, tables match but columns do not
@@ -21,6 +61,16 @@ class TestDataBaseConnections(unittest.TestCase):
     # print(f"groups: {db.TagManager.groups}")
     pass
 
+
+# print("\ntags: ")
+# for i in db.TagManager.tags.values():
+#     print(i)
+# print("\ngroups: ")
+# for i in db.TagManager.groups.values():
+#     print(i)
+# print("\nfiles: ")
+# for i in db.FileManager.files.values():
+#     print(i)
 
 # --- Operational Tests ---
 
